ab53ca6d340fdcf9319566b7bdaba3b750ecf206
Jeffrey Benchu Li <66740889+JeliPenguin@users.noreply.github.com>
2023-03-18 13:52:10 +0000

    updated noise level

--- Code/GuideAndScout/const.py
+++ Code/GuideAndScout/const.py
@@ -30,5 +30,6 @@ trainSeed = 10
 # Seed for randomness in envrionment init
 evalSeed = 20
 
-evalNoiseLevels = [0, 0.001, 0.005, 0.01,
-                            0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1]
+# evalNoiseLevels = [0, 0.001, 0.005, 0.01,0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1]
+
+evalNoiseLevels = [0, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
